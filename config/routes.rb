1dae8d4a2960cc8b71e4c2713d9ebf4b6e31cf4b
rua maruoka <rua.sun218@gmail.com>
2022-07-06 11:24:14 +0900

    修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
   resources :ints
   resources :labels
   resources :tasks
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -28,7 +28,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-
+         @task = FactoryBot.create(:task, title: 'aa', content: 'aa')
+         visit task_path(@task.id)
+         expect(page).to have_content 'aa'
        end
      end
   end
