164df36fc1ebe50cbe1d6468fc9bba02975aed29
rua maruoka <rua.sun218@gmail.com>
2022-07-11 11:52:15 +0900

    タスク一覧画面で、タスクを終了期限でソートできるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all.order(created_at: :DESC)
+    @tasks = Task.all.order(deadline: :DESC) if params[:sort_deadline]
   end
 
   # GET /tasks/1 or /tasks/1.json
