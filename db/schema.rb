905a9fb859aa6c981b872859a1a354d163ae9abe
rua maruoka <rua.sun218@gmail.com>
2022-07-14 20:42:22 +0900

    priorityのカラムの型を変更した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_14_061034) do
+ActiveRecord::Schema.define(version: 2022_07_14_113413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2022_07_14_061034) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.datetime "deadline"
-    t.string "priority"
+    t.integer "priority"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
