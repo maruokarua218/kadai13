2668d68239254565f9abba98341d112060e1d299
rua maruoka <rua.sun218@gmail.com>
2022-07-20 17:44:42 +0900

    ログイン機能を実装しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :tasks do
     member do
       get 'search', to: 'tasks#search'
